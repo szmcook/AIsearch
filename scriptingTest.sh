57c7ac082876f4b9c66b498999023fca8ed9a0a0
Sam Cook <samuel.j.cook@durham.ac.uk>
2021-01-04 16:41:00 +0000

    renamed files for submission

--- scriptingTest.sh
+++ scriptingTest.sh
@@ -37,32 +37,32 @@
 # python3 AlgAenhanced.py AISearchfile012.txt
 # echo "AlgAenhanced with 12 cities done"
 
-# python3 AlgAenhanced.py AISearchfile017.txt
-# echo "AlgAenhanced with 17 cities done"
+python3 AlgAenhanced.py AISearchfile017.txt
+echo "AlgAenhanced with 17 cities done"
 
-# python3 AlgAenhanced.py AISearchfile021.txt
-# echo "AlgAenhanced with 21 cities done"
+python3 AlgAenhanced.py AISearchfile021.txt
+echo "AlgAenhanced with 21 cities done"
 
-# python3 AlgAenhanced.py AISearchfile026.txt
-# echo "AlgAenhanced with 26 cities done"
+python3 AlgAenhanced.py AISearchfile026.txt
+echo "AlgAenhanced with 26 cities done"
 
-# python3 AlgAenhanced.py AISearchfile042.txt
-# echo "AlgAenhanced with 42 cities done"
+python3 AlgAenhanced.py AISearchfile042.txt
+echo "AlgAenhanced with 42 cities done"
 
-# python3 AlgAenhanced.py AISearchfile048.txt
-# echo "AlgAenhanced with 48 cities done"
+python3 AlgAenhanced.py AISearchfile048.txt
+echo "AlgAenhanced with 48 cities done"
 
-# python3 AlgAenhanced.py AISearchfile058.txt
-# echo "AlgAenhanced with 58 cities done"
+python3 AlgAenhanced.py AISearchfile058.txt
+echo "AlgAenhanced with 58 cities done"
 
-# python3 AlgAenhanced.py AISearchfile175.txt
-# echo "AlgAenhanced with 175 cities done"
+python3 AlgAenhanced.py AISearchfile175.txt
+echo "AlgAenhanced with 175 cities done"
 
-# python3 AlgAenhanced.py AISearchfile180.txt
-# echo "AlgAenhanced with 180 cities done"
+python3 AlgAenhanced.py AISearchfile180.txt
+echo "AlgAenhanced with 180 cities done"
 
-# python3 AlgAenhanced.py AISearchfile535.txt
-# echo "AlgAenhanced with 535 cities done"
+python3 AlgAenhanced.py AISearchfile535.txt
+echo "AlgAenhanced with 535 cities done"
 
 # # AlgBbasic
 # python3 AlgBbasic.py AISearchfile012.txt
@@ -127,32 +127,32 @@
 # echo "AlgBDD with 535 cities done"
 
 # AlgBenhanced
-python3 AlgBenhanced.py AISearchfile012.txt
-echo "AlgBenhanced with 12 cities done"
+# python3 AlgBenhanced.py AISearchfile012.txt
+# echo "AlgBenhanced with 12 cities done"
 
-python3 AlgBenhanced.py AISearchfile017.txt
-echo "AlgBenhanced with 17 cities done"
+# python3 AlgBenhanced.py AISearchfile017.txt
+# echo "AlgBenhanced with 17 cities done"
 
-python3 AlgBenhanced.py AISearchfile021.txt
-echo "AlgBenhanced with 21 cities done"
+# python3 AlgBenhanced.py AISearchfile021.txt
+# echo "AlgBenhanced with 21 cities done"
 
-python3 AlgBenhanced.py AISearchfile026.txt
-echo "AlgBenhanced with 26 cities done"
+# python3 AlgBenhanced.py AISearchfile026.txt
+# echo "AlgBenhanced with 26 cities done"
 
-python3 AlgBenhanced.py AISearchfile042.txt
-echo "AlgBenhanced with 42 cities done"
+# python3 AlgBenhanced.py AISearchfile042.txt
+# echo "AlgBenhanced with 42 cities done"
 
-python3 AlgBenhanced.py AISearchfile048.txt
-echo "AlgBenhanced with 48 cities done"
+# python3 AlgBenhanced.py AISearchfile048.txt
+# echo "AlgBenhanced with 48 cities done"
 
-python3 AlgBenhanced.py AISearchfile058.txt
-echo "AlgBenhanced with 58 cities done"
+# python3 AlgBenhanced.py AISearchfile058.txt
+# echo "AlgBenhanced with 58 cities done"
 
-python3 AlgBenhanced.py AISearchfile175.txt
-echo "AlgBenhanced with 175 cities done"
+# python3 AlgBenhanced.py AISearchfile175.txt
+# echo "AlgBenhanced with 175 cities done"
 
-python3 AlgBenhanced.py AISearchfile180.txt
-echo "AlgBenhanced with 180 cities done"
+# python3 AlgBenhanced.py AISearchfile180.txt
+# echo "AlgBenhanced with 180 cities done"
 
-python3 AlgBenhanced.py AISearchfile535.txt
-echo "AlgBenhanced with 535 cities done"
+# python3 AlgBenhanced.py AISearchfile535.txt
+# echo "AlgBenhanced with 535 cities done"
